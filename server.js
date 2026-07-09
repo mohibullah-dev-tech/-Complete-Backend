6c7857f508e16005a6fb0a5556c440da760ab533
Md Mohibullah <mdmohibullah352@gmail.com>
2026-07-09 19:41:16 +0600

    start server how to get

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express()
 
-app.listen(3000, () =>{
-    console.log("Server is running on port 3000");
+// app.listen(3000, () =>{
+//     console.log("Server is running on port 3000");
+// })
+
+app.get("/",(req,res) =>{
+    res.send("Hello World")
 })
+
+app.get("/about",(req,res) =>{
+    res.send("about page")
+})
+
+app.listen(3000)
