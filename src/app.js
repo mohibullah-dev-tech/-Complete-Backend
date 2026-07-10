9fd1275b1531cd816fc9536126e2e801d3fa7803
Md Mohibullah <mdmohibullah352@gmail.com>
2026-07-10 11:48:31 +0600

    added postman for build api

--- src/app.js
+++ src/app.js
@@ -4,4 +4,11 @@ const express = require("express")
 
 const  app = express()
 
+app.use(express.json()) // midelwere 
+const notes = []
+
+app.post('/notes',(req,res) =>{
+    console.log(req.body)
+})
+
 module.exports =app
