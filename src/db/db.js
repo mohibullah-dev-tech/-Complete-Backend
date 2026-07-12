555f2050b12dc8de30f9fc08c37afd6e3a607c74
Md Mohibullah <mdmohibullah352@gmail.com>
2026-07-12 16:16:08 +0600

    server create for new assignment

--- src/db/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-async function connectDB() {
-    await mongoose.connect("mongodb+srv://yt:fqDfzl8gq9myhRsi@yt-complete-backend.agnizig.mongodb.net/mohibullah")
-
-    console.log("connected to DB ");
-}
-
-module.exports = connectDB
