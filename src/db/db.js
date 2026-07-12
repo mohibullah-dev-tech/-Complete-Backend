6939ecac0dbe7d8e2f9ffdd4969801326d3b3283
Md Mohibullah <mdmohibullah352@gmail.com>
2026-07-12 22:36:45 +0600

    image post

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connectDB(){
-    await mongoose.connect("mongodb+srv://yt:fqDfzl8gq9myhRsi@yt-complete-backend.agnizig.mongodb.net/projects-1")
+    await mongoose.connect("mongodb+srv://yt:fqDfzl8gq9myhRsi@yt-complete-backend.agnizig.mongodb.net/projects-1");
     console.log("Connected to DB");
 }
 
