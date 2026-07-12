495040952f1f10ef129f0a47e09916ea64235e18
Md Mohibullah <mdmohibullah352@gmail.com>
2026-07-12 19:43:37 +0600

    server setup

--- src/db/db.js
+++ src/db/db.js
@@ -3,3 +3,5 @@ async function connectDB(){
     await mongoose.connect("mongodb+srv://yt:fqDfzl8gq9myhRsi@yt-complete-backend.agnizig.mongodb.net/projects-1")
     console.log("Connected to DB");
 }
+
+module.exports = connectDB;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
-const app = require("./src/app");
+const app = require("./app");
+
+const connectDB = require("./db/db");
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
